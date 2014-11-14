13616938daf6b61648e7d327d7ef3f9aad651353
Robson Vargas Farias <irobson@users.noreply.github.com>
2014-11-14 01:11:05 -0200

    remove comments

--- source/src/java/org/jovenslivres/controller/CustomRoutes.java
+++ source/src/java/org/jovenslivres/controller/CustomRoutes.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jovenslivres.controller;
 
 import br.com.caelum.vraptor.http.route.Router;
